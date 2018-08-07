07f30241db004e76e50baf86aa644bb071c869ea
injungkim <nebori92@gmail.com>
2018-08-07 21:05:08 +0900

    Initial Commit

---
+++ HDMC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HDMC
+//
+//  Created by injungkim on 07/08/2018.
+//  Copyright © 2018 injungkim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
