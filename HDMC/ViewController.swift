22995ed83f5a032b4838b3a0199305d163ab5678
injungkim <nebori92@gmail.com>
2018-08-07 21:52:57 +0900

    Add base WKWebview.

--- HDMC/ViewController.swift
+++ HDMC/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+// MARK: - Call app names
+let CALL_APP_QRCODE             = "QRcode"
 
+// MARK: - Call script names
+let CALL_SCRIPT_QRCode          = "QRCode()"
+
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler {
+    
+    // UI Components (_Member Variable)
+    // WKWebView before IOS 11.0 (NSCoding support was broken in previous versions)
+    var webView: WKWebView!
+    
+    // Member Variable
+    let contentController = WKUserContentController()
+    let configuration = WKWebViewConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Initialize WKWebView
+        configuration.userContentController = contentController
+        webView = WKWebView(frame: self.view.frame, configuration: configuration)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        
+        // JS -> Native Application
+        webView.configuration.userContentController.add(self, name: CALL_APP_QRCODE)
+        
+        // Native Application -> JS
+        webView.evaluateJavaScript(CALL_SCRIPT_QRCode) { (result, error) in
+            
+        }
+        
+        // Add subview
+        self.view.addSubview(webView)
+
+        // 초기 url 삽입
+        webView.load(URLRequest(url: URL(string: "https://google.com")!))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +54,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - WKScriptMessageHandler
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == CALL_APP_QRCODE {
+            // qrcode를 불렀을 때 동작
+        }
+    }
 
 }
-
